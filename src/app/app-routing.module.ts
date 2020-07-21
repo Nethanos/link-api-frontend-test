43a4453bdd1802a709a6c2761e421606c49bfc28
AdeildoNeto <n.santos1897@gmail.com>
2020-07-20 23:58:21 -0300

    [CONFIG] Commit inicial - Scaffold do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomePageComponent},
+  {path: "filmes", loadChildren:() => import('./pages/movies/movies.module').then(m => m.MoviesModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MoviesModule } from './pages/movies/movies.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomePageComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MoviesModule,
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
