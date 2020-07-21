13726bf8bf419f94fda90ef4a83cd9ffaff0dcc7
AdeildoNeto <n.santos1897@gmail.com>
2020-07-21 09:59:44 -0300

    [FEATURE] Finalizada fuzzy search

--- src/app/pages/movies/movies-home/movies.component.ts
+++ src/app/pages/movies/movies-home/movies.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { MoviesService } from '../../../service/movies.service';
+import { MoviesService } from '../../../services/movies.service';
 import { Movie } from '../../../models/movie';
 import { finalize } from 'rxjs/operators';
 
@@ -17,16 +17,36 @@ export class MoviesComponent implements OnInit, AfterViewInit {
 
   movieList: Array<Movie>;
 
-  ngOnInit(): void {
+  searchQuery: string;
 
+  hasMovies = true;
+
+  ngOnInit(): void {
   
   }
 
   ngAfterViewInit(): void {
     this.movieService.getMyMovies().pipe(finalize(() => this.loadingMovies = false) ).subscribe(response => {
-     if(response.length > 1) this.movieList = response;
+     if(response.length >= 1) this.movieList = response;
+
+     this.hasMovies = true;
       
     })
+  }
+
+  searchMovie(){
+
+   this.movieService.getMovieByTitle(this.searchQuery).subscribe(response => {
+
+     if(response.length >= 1){
+      this.movieList = response;
+      this.hasMovies = true;
+     }else {
+       this.hasMovies = false;
+     }
+
+
+   });
   }
 
 }
