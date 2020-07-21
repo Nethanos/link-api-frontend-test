cd695ce699531b02d376d4dd35eaf6b5ca7016e8
AdeildoNeto <n.santos1897@gmail.com>
2020-07-21 09:57:39 -0300

    [FEATURE][WIP] Validacao de formulario de profile

---
+++ src/app/helpers/genre-option-list.ts
@@ -0,0 +1,2 @@
+export const GenreOptionList = ['Action', 'Drama', 'Crime', 'Music', 'Sci-fi', 'Biograhy', 'Adventure', 'Thriller', 'Romance', 'Fantasy', 'Mystery',
+'Animation']
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Profile } from '../../models/profile';
+import { ProfileService } from '../../services/profile.service';
+import {GenreOptionList} from '../../helpers/genre-option-list';
 
 
 
@@ -11,22 +14,34 @@ import { Router } from '@angular/router';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private profileService: ProfileService) { }
 
+
+  profile: Profile;
+
+  genreList = GenreOptionList;
 
   ngOnInit(): void {
   }
 
   userForm = new FormGroup({
-    name: new FormControl(''),
-    age: new FormControl(''),
+    name: new FormControl('', [Validators.required,]),
+    age: new FormControl('', [Validators.required,  Validators.pattern("^[0-9]*$")]),
     favoriteMovie: new FormControl(''),
     favoriteGenre: new FormControl('')
   })
 
 
   saveUser(){
-    this.router.navigate(['/filmes']);
+    this.profile = {...this.userForm.value};
+
+    this.profileService.saveProfile(this.profile).subscribe((response) => {
+      console.log("Perfil criado");
+      this.router.navigate(['/filmes']);
+
+
+    })
+
   }
 
 }
